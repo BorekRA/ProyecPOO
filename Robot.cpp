19a187bda5060f42c56d0a8ebdc047802375aa19
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-10-21 15:40:56 -0500

    Update and rename Robot.cpp to Robot.h

--- Robot.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by utec on 18/10/19.
-//
-int main(){
-cout<<"Hola"
-}
---
+++ Robot.h
@@ -0,0 +1,14 @@
+//
+// Created by norel on 10/21/19.
+//
+
+#ifndef PROYECTOPOO_ROBOT_H
+#define PROYECTOPOO_ROBOT_H
+
+
+class Robot {
+
+};
+
+
+#endif //PROYECTOPOO_ROBOT_H
