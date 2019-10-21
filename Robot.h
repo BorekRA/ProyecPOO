ee83ea6a71208007ecd5870286acf5984b56b3d0
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-10-21 15:56:29 -0500

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -4,11 +4,31 @@
 
 #ifndef PROYECTOPOO_ROBOT_H
 #define PROYECTOPOO_ROBOT_H
-
-
+// ------poner using Clase=tipo; de todas las clases-----
+using producto=int;
+using slot=int;
+//--------------------------------------------------------
 class Robot {
-
+ private:
+  //----tipo int---
+  entero numero_robot;
+  //----Tipo doble---
+  deci peso;
+  deci bateria;
+  //---Tipo texto -----
+  str estado;
+  str producto_llevar;
+  //---tipo posicion--
+  posicion casa;
+  posicion y;
+  posicion x;
+ public:
+  estado();
+  posicion();//--para x e y---
+  consegur destino();
+  mover();
+  dar();
+  coger();
 };
-
-
+//--------------------------------------------------------
 #endif //PROYECTOPOO_ROBOT_H
