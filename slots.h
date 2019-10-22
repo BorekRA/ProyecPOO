d44fc0e4701a98c734ed6251bd0299cdee7cde22
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-10-22 14:08:18 -0500

    Create slots.h

---
+++ slots.h
@@ -0,0 +1,22 @@
+//
+// Created by Mate on 21/10/2019.
+//
+
+#ifndef SLOTS_SLOTS_H
+#define SLOTS_SLOTS_H
+
+
+class slots {
+private:
+    int forma;
+    int estado;
+    int dimensiones;
+    int contenido;
+public:
+    void guardar();
+    void abrirse();
+    void cerrar();
+};
+
+
+#endif //SLOTS_SLOTS_H
