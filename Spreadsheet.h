285156d879aed8a3ebe1e70031205a17c454edeb
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-10-25 16:55:07 -0500

    Create Spreadsheet.h

---
+++ Spreadsheet.h
@@ -0,0 +1,8 @@
+// Nombre del grupo: Rivas4Life
+// Miembros del grupo: López López, Leonardo Gabriel
+//                     Malla Carrillo, Mateo Rodrigo
+//                     Rodriguez Arce, Norel Rosendo
+// Responsabilidades:
+// Robot pt1, Almacen ----> Mateo Malla
+// Slots, Diagrama, Robot pt2 ----> Norel Rodriguez
+// Orden, Producto, Robot pt3 ----> Leonardo López
