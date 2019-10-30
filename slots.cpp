2bcd1b043aefc776c7f301104832565369c06959
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-10-30 18:35:06 -0500

    Update slots.cpp

--- slots.cpp
+++ slots.cpp
@@ -2,3 +2,13 @@
 using "slots.h"
 using "robots.h"
 using "producto.h"
+slots_t::slots_t(long ejeX,long ejeY):ejeX{x},ejeY{y} {
+ ejeX=x;
+ ejeY=y;
+}
+longitud slots_t::get_ejeX(){
+ return ejeX; 
+}
+longitud slots_t::get_ejeY(){
+ return ejeY; 
+}
