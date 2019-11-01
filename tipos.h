b2643fd82f10cf4df66b102f62da36d376ea68c0
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-01 15:57:51 -0500

    Create tipos.h

---
+++ tipos.h
@@ -0,0 +1,13 @@
+#ifndef PROYECPOO_TIPOS_H
+#define PROYECPOO_TIPOS_H
+#include<iostream>
+#include <vector>
+#include<string>
+using namespace std;
+using longitud=int;
+using posicion=size_t;
+using decimal=double;
+using texto=string;
+using valor=unsigned int;
+
+#endif //PROYECPOO_TIPOS_H
