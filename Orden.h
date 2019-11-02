9cff245376362f73adc895cab646b825048aca45
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-11-01 20:51:17 -0500

    Create Orden.h

---
+++ Orden.h
@@ -0,0 +1,25 @@
+//
+// Created by norel on 11/1/19.
+//
+
+#ifndef PROYECTOPOO_ORDEN_H
+#define PROYECTOPOO_ORDEN_H
+
+#include <string>
+#include <ionstream>
+using namespace std;
+typedef int Posx;
+typedef int Posy;
+typedef entero;
+typedef int prioridad;
+class Orden {
+private:
+    prioridad prioridad1;
+public:
+    set_prioridad();
+    posicionFinal(x,y);
+    get_posicionFinal
+};
+
+
+#endif //PROYECTOPOO_ORDEN_H
