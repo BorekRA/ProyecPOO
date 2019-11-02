718410f78d450cb9449ecaa676b1a678b3541a07
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-01 21:31:22 -0500

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -6,30 +6,32 @@
 #define PROYECTOPOO_ROBOT_H
 #include "tipos.h"
 class Crobot {
-  private:
-  /*
-  posicion x1; //para settear la posicion de la casa del robot
-  posicion y1;
-  */
-  matrix_t casa;
-  valor numero; //numero de robot
-  /*
-  posicion x2; // para el destino
-  posicion y2;
-  */
-  matrix_t destino
-  banderita estado;
-  valor bateria;
-  public:
-  Crobot(valor num, matrix_t cs, valor btr, banderita stad); 
-  void set_casa(matrix_t cs);
-  void set_destino(matrix_t dest);
-  void set_estado(banderita stad);
-  void set_bateria();
-  Crobot get_bateria();
-  Crobot get_orden();
-  void recargar_bateria();
-  void set_orden(valor odr);
-  
+private:
+    /*
+    posicion x1; //para settear la posicion de la casa del robot
+    posicion y1;
+    */
+    matrix_t casa;
+    valor numero; //numero de robot
+    /*
+    posicion x2; // para el destino
+    posicion y2;
+    */
+    matrix_t destino;
+    banderita estado;
+    valor bateria;
+    valor orden;
+public:
+    Crobot(valor num, matrix_t cs, valor btr, banderita stad);
+    valor get_bateria();
+    valor get_orden();
+    banderita set_estado(banderita stad);
+    void set_casa(matrix_t cs);
+    void set_destino(matrix_t dest);
+    void set_bateria();
+    void recargar_bateria();
+    void set_orden(valor odr);
+
 };
+
 #endif //PROYECTOPOO_ROBOT_H
