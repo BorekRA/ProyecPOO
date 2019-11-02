fd1e92e9ae5940f5de00f329b2a65521e6836790
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-11-01 20:50:58 -0500

    Create Orden.cpp

---
+++ Orden.cpp
@@ -0,0 +1,14 @@
+//
+// Created by norel on 11/1/19.
+//
+#include "Orden.h"
+int Orden::set_prioridad() {
+    
+}
+int Orden::get_posicionFinal(){
+    return poscionFinal(x,y);
+}
+Orden::PosicionFinal(x,y){
+    Posx=x;
+    Posy=y;
+}
