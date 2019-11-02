771c51f9b354db4e27fc4874e0c26192f84ae9fd
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-01 21:29:22 -0500

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,7 +1,54 @@
-//---------- poner todos los include --------------
 #include "Robot.h"
-#include "Slot.cpp"
 #include "Slot.h"
 #include "Producto.h"
-#include "Producto.cpp"
-//--------------------------
+Crobot::Crobot(valor num, matrix_t cs, valor btr, banderita stad):numero(num), casa(cs), estado (stad){
+
+}
+
+valor Crobot::get_bateria() {
+    return bateria;
+}
+
+valor Crobot::get_orden() {
+    return orden;
+}
+
+void Crobot::set_casa(matrix_t cs) {
+valor x,y;
+cin>>x;
+cin>>y;
+cs[x][y];
+casa=cs;
+}
+
+void Crobot::set_destino(matrix_t dest) {
+    valor x,y;
+    cin>>x;
+    cin>>y;
+    dest[x][y];
+    destino=dest;
+}
+
+banderita Crobot::set_estado(banderita stad) {
+    if(estado="ocupado"){
+        stad=false;
+    }
+    else{
+        stad=true;
+    }
+}
+
+void Crobot::set_bateria() {
+valor num_mov;
+    if (num_mov==5)bateria-=5;
+}
+
+void Crobot::recargar_bateria() {
+if(bateria==15)
+    bateria=100;
+}
+
+void Crobot::set_orden(valor odr) {
+orden=odr;
+}
+
