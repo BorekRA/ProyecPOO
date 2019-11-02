5eada0b629b5ea35742ea731f8eaf815aba2a56d
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-01 19:01:24 -0500

    Update producto.h

--- producto.h
+++ producto.h
@@ -5,20 +5,34 @@
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
 
+#include <string>
+#include <iostream>
+using namespace std;
 typedef int Type_e;
 typedef double Type_d;
 typedef char Type_c;
 typedef unsigned long int  Type_l;
 typedef std::string Type_s;
-
+typedef int Type_i;
 class Cproducto{
-  private:
-    Type_l código; //codigo del producto
-    Type_d peso; 
-    Type_d tamagno; 
+private:
+    Type_l codigo;
+    Type_d peso; //estará en kilogramos segun la cantidad de productos que tenga
+    Type_d pesounitario;
+    Type_d tamagno;
     Type_s fechaprod;  //fecha de producción
-  public:
-    Type_e adinv(); //Aumentar o Disminuir Inv
+    Type_e cantidad; //cantidad del producto
+    /*codigo del producto El codigo sera
+     de 3 digitos (por ahora), los dos primeros números corresponden al nuemero de la letra de la primera letra del nombre del producto, el tercer y cuarto
+     digito sera el numero de la ultima letra del nombre del producto más el valor de los ods primeros digitos, por ejemplo agua serà 0102*/
+public:
+    Cproducto(Type_l cod, Type_d tam,Type_d pesounitario, Type_e cantidad, Type_s fecha);
+    Type_l get_codigo();
+    Type_d get_peso();
+    Type_d get_tamagno();
+    Type_s get_fecha();
+    Type_e get_cantidad();
+    void set_cantidad(Type_i cant);
 };
 
 #endif
