516ee6de93309f0301ff813022561d1bc5fda4ec
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-01 21:37:58 -0500

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -23,10 +23,12 @@ casa=cs;
 
 void Crobot::set_destino(matrix_t dest) {
     valor x,y;
+    if(dest==casa) destino=casa;
+    else{
     cin>>x;
     cin>>y;
     dest[x][y];
-    destino=dest;
+    destino=dest;}
 }
 
 banderita Crobot::set_estado(banderita stad) {
@@ -42,10 +44,11 @@ void Crobot::set_bateria() {
 valor num_mov;
     if (num_mov==5)bateria-=5;
 }
-
 void Crobot::recargar_bateria() {
-if(bateria==15)
-    bateria=100;
+    if(bateria==10) {
+        set_destino(casa);
+        bateria = 100;
+    }
 }
 
 void Crobot::set_orden(valor odr) {
