3bd8ac06b9ed7ccee35b58538d7e8d3cb0978265
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-22 17:06:43 -0500

    Update almacen.h

--- almacen.h
+++ almacen.h
@@ -1,31 +1,19 @@
-//
-// Created by Mate on 31/10/2019.
-//
-
-#ifndef PROYECPOO_ALMACEN_H
-#define PROYECPOO_ALMACEN_H
-#include "robot.h"
-#include "slots.h"
-#include "producto.h"
-#include <SMFL/Graphics/RenderWindow.hpp>
+//#include "robot.h"
+//#include "slots.h"
+//#include "producto.h"
 #include "tipos.h"
 
 class Calmacen {
-private:
     longitud altura;
     longitud ancho;
     valor col;
     valor fil;
-    sf::RenderWindows* canvas;
-    mtr slots;
-    vtr producto;
-    void draw_rectangle(posicion a, posicion b, longitud x, longitud y);
+    vector<vector<texto>> slots;
 public:
-    void get_slots(valor x,valor y);
-    almacen(longitud x, longitud y, valor fl, valor cl, sf::RenderWindow *cnv);
-    void draw();
-
+    Calmacen(valor c,valor f);
+    void imprimir_almacen_vacio();
+    void almacen_vacio();
+    //void agregar_slot(CSlot &ptrslot);
+    virtual ~Calmacen()=default;
 };
 
-
-#endif //PROYECPOO_ALMACEN_H
