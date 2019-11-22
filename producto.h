b744c2ee0fb34ae8302ddcf02b418d51c48cf71b
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-22 17:21:17 -0500

    Update producto.h

--- producto.h
+++ producto.h
@@ -5,34 +5,32 @@
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
 
-#include <string>
-#include <iostream>
+#include "tipos.h"
 using namespace std;
-typedef int Type_e;
-typedef double Type_d;
-typedef char Type_c;
-typedef unsigned long int  Type_l;
-typedef std::string Type_s;
-typedef int Type_i;
+
 class Cproducto{
 private:
-    Type_l codigo;
-    Type_d peso; //estará en kilogramos segun la cantidad de productos que tenga
-    Type_d pesounitario;
-    Type_d tamagno;
-    Type_s fechaprod;  //fecha de producción
-    Type_e cantidad; //cantidad del producto
+    t_texto codigo;
+    t_decimal peso; //estará en kilogramos segun la cantidad de productos que tenga
+    t_decimal pesounitario;
+    const t_decimal pesomax=10;
+    t_texto fechaprod;  //fecha de producción
+    t_ent cantidad; //cantidad del producto
+    t_texto posicion;
+
     /*codigo del producto El codigo sera
      de 3 digitos (por ahora), los dos primeros números corresponden al nuemero de la letra de la primera letra del nombre del producto, el tercer y cuarto
-     digito sera el numero de la ultima letra del nombre del producto más el valor de los ods primeros digitos, por ejemplo agua serà 0102*/
+     digito sera el numero de la ultima letra del nombre del producto más el valor de los dos primeros digitos, por ejemplo agua serà 0102*/
 public:
-    Cproducto(Type_l cod, Type_d tam,Type_d pesounitario, Type_e cantidad, Type_s fecha);
-    Type_l get_codigo();
-    Type_d get_peso();
-    Type_d get_tamagno();
-    Type_s get_fecha();
-    Type_e get_cantidad();
-    void set_cantidad(Type_i cant);
+    Cproducto(t_texto cod,t_decimal punit, t_ent cant, t_texto fecha, t_texto pos);
+    t_texto get_codigo();
+    t_texto get_posicion();
+    t_decimal get_peso();
+    t_texto get_fecha();
+    t_ent get_cantidad();
+    void set_cantidad(t_ent cant);
+    void sumar_restar_cantidad( t_ent cant);
+    void redistribuir_carga ();
 };
 
 #endif
