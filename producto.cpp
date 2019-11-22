7876eab25f12b3727892c192d067274905a88ced
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-22 17:11:30 -0500

    Update producto.cpp

--- producto.cpp
+++ producto.cpp
@@ -1,33 +1,51 @@
 #include "producto.h"
 
-Cproducto::Cproducto(Type_l cod, Type_d tam,Type_d punit, Type_e cant, Type_s fecha)
-:codigo(cod),tamagno(tam), fechaprod(fecha),cantidad(cant),pesounitario(punit){
+Cproducto::Cproducto(t_texto cod, t_decimal punit, t_ent cant, t_texto fecha, t_texto pos):codigo(cod),fechaprod(fecha),
+cantidad(cant),pesounitario(punit), posicion(pos) {
 
 }
 
-void Cproducto::set_cantidad(Type_i cant) {
-cantidad=+cant; //falta agregar el limite de productos que pueden caber en el contenedor por el tamañ que tiene cada producto
-}
-
-
-Type_l Cproducto::get_codigo() {
+t_texto Cproducto::get_codigo() {
     return codigo;
 }
 
-Type_d Cproducto::get_peso() {
-    peso=cantidad*pesounitario; //Falta agregar el valor de peso constante que pesa el contenedor
+t_decimal Cproducto::get_peso() {
+    peso=pesounitario*cantidad;
     return peso;
 }
 
-Type_d Cproducto::get_tamagno() {
-    return tamagno;
-}
-
-Type_s Cproducto::get_fecha() {
+t_texto Cproducto::get_fecha() {
     return fechaprod;
 }
 
-Type_e Cproducto::get_cantidad() {
+t_ent Cproducto::get_cantidad() {
     return cantidad;
 }
 
+void Cproducto::set_cantidad(t_ent cant) {
+cantidad=cant;
+}
+
+void Cproducto::sumar_restar_cantidad(t_ent cant) {
+cantidad+=cant;
+redistribuir_carga();
+}
+
+void Cproducto::redistribuir_carga() {
+    int exceso;
+    int cant1=cantidad;
+    int cont;
+    if (peso<pesomax) {
+        for (double i = 0; i < pesomax; i++) {
+            i += pesounitario;
+            cont += 1;
+        }
+        exceso = cant1 - cont;
+        cout<<"Hay"<<exceso<<"productos que exceden la carga máxima permitida. Favor de ingresar una cantidad menor";
+        set_cantidad(cont);
+    }
+}
+
+t_texto Cproducto::get_posicion() {
+    return t_texto();
+}
