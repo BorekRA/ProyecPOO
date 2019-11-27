9d1b8654e78e622c2837a1bd418439b49d735f6f
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-27 14:53:18 -0500

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -1,37 +1,28 @@
-//
-// Created by norel on 10/21/19.
-//
+#ifndef PROYECTO_POO_ROBOT_H
+#define PROYECTO_POO_ROBOT_H
 
-#ifndef PROYECTOPOO_ROBOT_H
-#define PROYECTOPOO_ROBOT_H
 #include "tipos.h"
-class Crobot {
+#include "orden.h"
+#include "producto.h"
+
+class CRobot {
 private:
-    /*
-    posicion x1; //para settear la posicion de la casa del robot
-    posicion y1;
-    */
-    matrix_t casa;
-    valor numero; //numero de robot
-    /*
-    posicion x2; // para el destino
-    posicion y2;
-    */
-    matrix_t destino;
-    banderita estado;
-    valor bateria;
-    valor orden;
+    posicion pos_x;
+    posicion pos_y;
+    matrix_t home;
+    t_ent name;
+    verificador state= true;
+    t_ent battery;
+    COrden* order;
 public:
-    Crobot(valor num, matrix_t cs, valor btr, banderita stad);
-    valor get_bateria();
-    valor get_orden();
-    banderita set_estado(banderita stad);
-    void set_casa(matrix_t cs);
-    void set_destino(matrix_t dest);
-    void set_bateria();
-    void recargar_bateria();
-    void set_orden(valor odr);
+    CRobot(t_ent nam, t_posicion x, t_posicion y);
+    t_ent get_battery();
+    banderita set_state(banderita stad);
+    void recharge_battery();
+    void set_order(COrden* p_order);
+    void alternate_state();
+
 
 };
+#endif //PROYECTO_POO_ROBOT_H
 
-#endif //PROYECTOPOO_ROBOT_H
