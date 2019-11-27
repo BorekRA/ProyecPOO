dff2c49d2c3008ba861ae79634eced2ae13a6671
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-27 14:55:17 -0500

    Update producto.h

--- producto.h
+++ producto.h
@@ -1,12 +1,10 @@
-//
-//Created by Lopez 10/21/2019
-//
-
-#ifndef PRODUCTO_H
-#define PRODUCTO_H
+#ifndef PROYECTO_POO_PRODUCTO_H
+#define PROYECTO_POO_PRODUCTO_H
 
 #include "tipos.h"
-using namespace std;
+#include "robot.h"
+#include "almacen.h"
+#include "orden.h"
 
 class Cproducto{
 private:
@@ -18,9 +16,6 @@ private:
     t_ent cantidad; //cantidad del producto
     t_texto posicion;
 
-    /*codigo del producto El codigo sera
-     de 3 digitos (por ahora), los dos primeros números corresponden al nuemero de la letra de la primera letra del nombre del producto, el tercer y cuarto
-     digito sera el numero de la ultima letra del nombre del producto más el valor de los dos primeros digitos, por ejemplo agua serà 0102*/
 public:
     Cproducto(t_texto cod,t_decimal punit, t_ent cant, t_texto fecha, t_texto pos);
     t_texto get_codigo();
@@ -28,9 +23,10 @@ public:
     t_decimal get_peso();
     t_texto get_fecha();
     t_ent get_cantidad();
-    void set_cantidad(t_ent cant);
     void sumar_restar_cantidad( t_ent cant);
     void redistribuir_carga ();
+    void set_cantidad(t_ent cant);
 };
 
-#endif
+
+#endif //PROYECTO_POO_PRODUCTO_H
