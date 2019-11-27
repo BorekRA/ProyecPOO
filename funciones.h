a1bed3883612fceea25496304b155c272be7b3f3
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-26 23:07:25 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,7 @@
+#include "tipos.h"
+void menu_principal(); //Menu de Bienvenida del programa;
+void agregar_robots();
+void agregar_productos();
+void print_almacen();
+void ejecucion_de_ordenes(); // Menú de ejecuccion;
+void print_salida_del_programa(); //Menú de salida;
