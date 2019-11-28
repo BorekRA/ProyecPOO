c12d9d1a081b7f557a075ab04edddadee0cb52dc
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 00:06:50 -0500

    Update Cproducto.h

--- Cproducto.h
+++ Cproducto.h
@@ -6,13 +6,18 @@
 class Cproducto{
     texto nombre;
     let letra;
+    posicion posx;
+    posicion posy;
 public:
-    Cproducto(texto n,let l);
+    Cproducto(texto n,let l, posicion x, posicion y);
     void set_nombre(texto n);
+    valor get_x(){return posx;};
+    valor get_y(){return posy;};
     void set_letra(let l);
     texto get_nombre();
     let get_letra();
 };
+
 
 
 #endif //PROYECTO_POO_PRODUCTO_H
