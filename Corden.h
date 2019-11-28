75cf0757eaa4526044f682167cd0ccf7e7073342
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-27 22:16:43 -0500

    Update Corden.h

--- Corden.h
+++ Corden.h
@@ -2,17 +2,20 @@
 #define PROYECTO_POO_ORDEN_H
   
 #include "tipos.h"
-class Corden{
-    valor posx;
-    valor posy;
-
+class COrden {
+private:
+    //t_ent prioridad;  aun en implementacion
+    t_posicion pos_x;
+    t_posicion pos_y;
+    CRobot* robot=nullptr;
+    Cproducto* producto= nullptr;
 public:
-    Corden(valor x, valor y){
-        posx=x;
-        posy=y;
-    }
-    valor get_x(){return posx;};
-    valor get_y(){return posy;};
+    //void set_prioridad (t_ent prior);
+    void execute_order(t_ent n, CRobot* rob,Cproducto* prod, t_posicion x, t_posicion y);
+    void recoger_producto(t_posicion x, t_posicion y);
+    void almacenar_producto(t_posicion x, t_posicion y);
+    void set_recharge(t_posicion x, t_posicion y);
+    //void sumar_restar_cantidad( t_ent cant, Cproducto prod);
 
 };
 
