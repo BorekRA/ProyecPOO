b87311a9f89fd009e3b3393b804a308a46082c5e
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-27 20:02:02 -0500

    Update and rename almacen.h to Calmacen.h

---
+++ Calmacen.h
@@ -0,0 +1,24 @@
+//#include "robot.h"
+//#include "slots.h"
+//#include "producto.h"
+#include "tipos.h"
+#include "Corden.h"
+#include "Cproducto.h"
+class Calmacen {
+    longitud altura;
+    longitud ancho;
+    valor col;
+    valor fil;
+    vector<vector<texto>> slots;
+public:
+    Calmacen(valor c,valor f);
+    void imprimir_almacen_vacio();
+    void almacen_vacio();
+    void agregar_productos(Cproducto P,Corden O);
+    //void agregar_robots(Crobot& ptrR);
+    virtual ~Calmacen()=default;
+    void imprimir_almacen();
+};
+
+Calmacen crear_almacen(istream& in);
+void indicar_productos(Calmacen& A1, istream& in);
