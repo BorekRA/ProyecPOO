2c35aa1c561a29a45826b30f91cd54155ff44250
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-27 20:04:55 -0500

    Update producto.cpp

--- producto.cpp
+++ producto.cpp
@@ -1,47 +1,25 @@
-#include "producto.h"
+#include "Cproducto.h"
+#include "tipos.h"
+Cproducto::Cproducto(texto n, let l):nombre{n},letra{l}{
 
-Cproducto::Cproducto(t_texto cod, t_decimal punit, t_ent cant, t_texto fecha, t_texto pos):codigo(cod), pesounitario(punit)
-, cantidad(cant), fechaprod(fecha), posicion(pos){
 }
 
-t_texto Cproducto::get_codigo() {
-    return codigo;
+void Cproducto::set_nombre(texto n) {
+    cout<<"Indique el nombre del producto: ";
+    cin>>n;
+    nombre=n;
 }
 
-t_texto Cproducto::get_posicion() {
-    return posicion;
+texto Cproducto::get_nombre() {
+    return nombre;
 }
 
-t_decimal Cproducto::get_peso() {
-    return peso;
+void Cproducto::set_letra(let l) {
+    cout<<"Indique la letra para representar el producto: ";
+    cin>>l;
+    letra=l;
 }
 
-t_texto Cproducto::get_fecha() {
-    return fechaprod;
-}
-
-t_ent Cproducto::get_cantidad() {
-    return cantidad;
-}
-void Cproducto::set_cantidad(t_ent cant){
-    cantidad=cant;
-};
-
-void Cproducto::sumar_restar_cantidad(t_ent cant) {
-cantidad+=cant;
-}
-
-void Cproducto::redistribuir_carga() {
-    int exceso;
-    int cant1=cantidad;
-    int cont;
-    if (peso<pesomax) {
-        for (double i = 0; i < pesomax; i++) {
-            i += pesounitario;
-            cont += 1;
-        }
-        exceso = cant1 - cont;
-        cout<<"Hay"<<exceso<<"productos que exceden la carga máxima permitida. Favor de ingresar una cantidad menor";
-        set_cantidad(cont);
-    }
+let Cproducto::get_letra() {
+    return letra;
 }
