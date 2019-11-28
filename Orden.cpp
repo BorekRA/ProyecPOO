7c484c4e2d253ade0e8ef2d836e18dc06280b354
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-27 19:58:32 -0500

    Update Orden.cpp

--- Orden.cpp
+++ Orden.cpp
@@ -1,14 +1,1 @@
-#include "tipos.h"
-class Corden{
-    valor posx;
-    valor posy;
-
-public:
-    Corden(valor x, valor y){
-        posx=x;
-        posy=y;
-    }
-    valor get_x(){return posx;};
-    valor get_y(){return posy;};
-
-};
+#include "Corden.h"
