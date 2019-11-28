8236264ce7eb6101c0496520fa5659392d2fa95f
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 13:31:04 -0500

    Update tipos.h

--- tipos.h
+++ tipos.h
@@ -11,6 +11,7 @@ using namespace std;
 using longitud=int;
 using cantidad=int;
 using posicion=size_t;
+using t_posicion=unsigned int;
 using decimal=double;
 using texto=string;
 using valor=unsigned int;
@@ -19,5 +20,7 @@ typedef vector<int> row_t;
 typedef vector<row_t> matrix_t;
 typedef bool banderita;
 using let=char;
+using t_ent=int;
+using verificador=bool;
 
 #endif //PROYECTO_POO_TIPOS_H
