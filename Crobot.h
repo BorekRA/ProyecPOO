1bb920c037b4c48ecee29428ed93d712824b8246
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 13:29:53 -0500

    Update Crobot.h

--- Crobot.h
+++ Crobot.h
@@ -2,25 +2,13 @@
 #define PROYECTO_POO_ROBOT_H
 #include "tipos.h"
 #include "Corden.h"
-#include "Cproducto.h
+
 class Crobot {
 private:
     posicion pos_x;
     posicion pos_y;
-    matrix_t home;
-    matrix_t current_location;
-    texto name;
-    verificador state= true;
-    t_ent battery;
     texto nombre;
 public:
-    Crobot(texto nam, t_posicion x, t_posicion y);
-    t_ent get_battery();
-    banderita set_state(banderita stad);
-    void recharge_battery();
-    void alternate_state();
-    void mover_robot(t_posicion x, t_posicion y);
-    texto get_nombre(){return name;}
     Crobot(texto nam, posicion x, posicion y){
         nombre=nam;
         pos_x=x;
@@ -29,9 +17,6 @@ public:
     texto get_nombre(){return nombre;}
     valor get_x(){return pos_x;};
     valor get_y(){return pos_y;};
-};
-
-};
 
 };
 
