f6d9e4f357dd78606d58160778d4b333abbcecb8
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 00:04:15 -0500

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -2,29 +2,30 @@
 #define PROYECTO_POO_ROBOT_H
 
 #include "tipos.h"
-#include "orden.h"
-#include "producto.h"
+#include "Corden.h"
+#include "Cproducto.h"
 
-class CRobot {
+class Crobot {
 private:
     posicion pos_x;
     posicion pos_y;
     matrix_t home;
     matrix_t current_location;
-    t_ent name;
+    texto name;
     verificador state= true;
     t_ent battery;
-    COrden* order;
 public:
-    CRobot(t_ent nam, t_posicion x, t_posicion y);
+    Crobot(texto nam, t_posicion x, t_posicion y);
     t_ent get_battery();
     banderita set_state(banderita stad);
     void recharge_battery();
-    void set_order(COrden* p_order);
     void alternate_state();
     void mover_robot(t_posicion x, t_posicion y);
-
-
+    texto get_nombre(){return name;}
+    valor get_x(){return pos_x;};
+    valor get_y(){return pos_y;};
 };
+
+
 #endif //PROYECTO_POO_ROBOT_H
 
