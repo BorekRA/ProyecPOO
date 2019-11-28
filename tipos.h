9c7e747a122248c4a1d1eac87064d1237cf3f8cd
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-27 20:03:11 -0500

    Update tipos.h

--- tipos.h
+++ tipos.h
@@ -6,9 +6,10 @@
 #include <vector>
 #include<string>
 #include <iomanip>
-#include <cstdlib>
+
 using namespace std;
 using longitud=int;
+using cantidad=int;
 using posicion=size_t;
 using decimal=double;
 using texto=string;
@@ -17,13 +18,6 @@ typedef vector<vector<int>> vetr;
 typedef vector<int> row_t;
 typedef vector<row_t> matrix_t;
 typedef bool banderita;
-typedef bool verificador;
-typedef int t_ent;
-typedef double t_decimal;
-typedef std::string t_texto;
-typedef int t_posicion;
-
-
-using namespace std;
+using let=char;
 
 #endif //PROYECTO_POO_TIPOS_H
