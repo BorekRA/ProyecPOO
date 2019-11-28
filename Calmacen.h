bb872eee4e528d4de831db25f8ea6b21733c529a
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 11:02:21 -0500

    Update Calmacen.h

--- Calmacen.h
+++ Calmacen.h
@@ -11,6 +11,8 @@ public:
     void imprimir_almacen_vacio();
     void almacen_vacio();
     void agregar_productos(Cproducto P);
+    valor get_col(){return col;}
+    valor get_fil(){return fil;}
     void agregar_robot(Crobot rt);
     virtual ~Calmacen()=default;
     void imprimir_almacen();
@@ -18,20 +20,4 @@ public:
 
 Calmacen crear_almacen(istream& in);
 void indicar_productos(Calmacen& A1, istream& in);
-void robots(Calmacen& A1,istream& in);
-
-/* Main que estoy utilizando
-#include "tipos.h"
-#include "Calmacen.h"
-
-int main() {
-    Calmacen A1=crear_almacen(cin);
-    A1.almacen_vacio();
-    A1.imprimir_almacen_vacio();
-    indicar_productos(A1,cin);
-    A1.imprimir_almacen();
-    robots(A1,cin);
-    A1.imprimir_almacen();
-    return 0;
-}*/
-
+void indicar_robots(Calmacen& A1,istream& in);
