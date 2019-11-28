8503eb047ac060b6e074745713bc639d8779f6b7
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 00:04:46 -0500

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,32 +1,28 @@
-#include "robot.h"
-#include "producto.h"
-CRobot::CRobot(t_ent nam, t_posicion x, t_posicion y):name(nam), pos_x(x), pos_y(y) {
-battery=100;
-order = nullptr;
-home[x][y];
+#include "Crobot.h"
+#include "Cproducto.h"
+Crobot::Crobot(texto nam, t_posicion x, t_posicion y):name{nam}, pos_x{x}, pos_y{y} {
+    battery=100;
+    home[x][y];
 }
 
-t_ent CRobot::get_battery() {
+t_ent Crobot::get_battery() {
     return battery;
 }
 
-banderita CRobot::set_state(banderita stad) {
+banderita Crobot::set_state(banderita stad) {
     return state;
 }
 
-void CRobot::recharge_battery() {
-if (battery==10) battery=100;
+void Crobot::recharge_battery() {
+    if (battery==10) battery=100;
 }
 
-void CRobot::set_order(COrden *p_order) {
-order=p_order;
-}
 
-void CRobot::alternate_state() {
+void Crobot::alternate_state() {
     if (state != 0) state=false;
     if (state == 0) state=true;
 }
-void CRobot::mover_robot(t_posicion x, t_posicion y){
-pos_x=x; pos_y=y;
+void Crobot::mover_robot(t_posicion x, t_posicion y){
+    pos_x=x; pos_y=y;
     current_location[pos_x][pos_y];
 }
