5fca477733e7be2707fd6465de970c966f8547ae
LeonardoL57 <54490467+LeonardoL57@users.noreply.github.com>
2019-11-27 22:17:19 -0500

    Update Corden.cpp

--- Corden.cpp
+++ Corden.cpp
@@ -1,1 +1,21 @@
 #include "Corden.h"
+void COrden::execute_order(t_ent n, CRobot* rob, Cproducto* prod,t_posicion x, t_posicion y){
+    robot=rob;
+    producto=prod;
+    switch (n){
+        case 1: set_recharge(x,y);
+        case 2: recoger_producto(x, y);
+        case 3: almacenar_producto(x,y);
+    }
+}
+void COrden::recoger_producto(t_posicion x, t_posicion y){
+    robot->mover_robot(x, y);
+    producto->mover_producto(x, y);
+};
+void COrden::almacenar_producto(t_posicion x, t_posicion y){
+    robot->mover_robot(x, y);
+    producto->mover_producto(0, 0);
+};
+
+void COrden::set_recharge(t_posicion x, t_posicion y) {
+    robot->mover_robot(x, y);
