49cc28562a3cb3fba5bb8f769b4ff9340d9251bc
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 00:05:54 -0500

    Update Calmacen.h

--- Calmacen.h
+++ Calmacen.h
@@ -1,9 +1,8 @@
 #include "tipos.h"
 #include "Corden.h"
 #include "Cproducto.h"
+#include "Crobot.h"
 class Calmacen {
-    longitud altura;
-    longitud ancho;
     valor col;
     valor fil;
     vector<vector<texto>> slots;
@@ -11,11 +10,12 @@ public:
     Calmacen(valor c,valor f);
     void imprimir_almacen_vacio();
     void almacen_vacio();
-    void agregar_productos(Cproducto P,Corden O);
-    //void agregar_robots(Crobot& ptrR);
+    void agregar_productos(Cproducto P);
+    void agregar_robot(Crobot rt);
     virtual ~Calmacen()=default;
     void imprimir_almacen();
 };
 
 Calmacen crear_almacen(istream& in);
 void indicar_productos(Calmacen& A1, istream& in);
+void robots(Calmacen& A1,istream& in);
