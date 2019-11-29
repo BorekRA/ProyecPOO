51409b504d8b0b004fc9e9f7c3cf78f95a545707
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-11-28 16:52:58 -0800

    Update Crobot.h

--- Crobot.h
+++ Crobot.h
@@ -1,13 +1,13 @@
 #ifndef PROYECTO_POO_ROBOT_H
 #define PROYECTO_POO_ROBOT_H
 #include "tipos.h"
-#include "Corden.h"
 
 class Crobot {
 private:
     posicion pos_x;
     posicion pos_y;
     texto nombre;
+
 public:
     Crobot(texto nam, posicion x, posicion y){
         nombre=nam;
@@ -17,8 +17,8 @@ public:
     texto get_nombre(){return nombre;}
     valor get_x(){return pos_x;};
     valor get_y(){return pos_y;};
+    void set_posicion_r(valor x, valor y, vector<vector<texto>> s);
 
 };
-
 #endif //PROYECTO_POO_ROBOT_H
 
