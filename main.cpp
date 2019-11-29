fa48ddb0f71367e787c72482399cf9b762b50685
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 21:59:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
-#include <iostream>
 #include "tipos.h"
 #include "Calmacen.h"
+
 int main() {
-        menu_principal();
-        Calmacen A1=crear_almacen(cin);
-        A1.almacen_vacio();
-        A1.imprimir_almacen_vacio();
-        indicar_robots(A1, cin);//agregar robots
-        A1.imprimir_almacen();
-        bool continuar = true;
-        while (continuar == true) {
-            t_ent operacion = 0;
-            cout << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM" << endl;
-            cout << "Que operacion desea hacer??  " << endl;
-            cout << "1.- Ingresar un producto" << endl;
-            cout << "2.- Retirar un producto" << endl;
-            cout << "3.- Estado del Robot" << endl;
-            cout << "4.- Imprimir Almacen" << endl;
-            cout <<" 5.- Cerrar el progrma"<<endl;
-            cout << "Indica el número de la operación" << endl;
-            cout << "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW" << endl;
-            cin >> operacion;
-            switch (operacion) {
-                case 1:
-                    indicar_productos(A1, cin);//agregar productos
-                    A1.imprimir_almacen();
-                case 4:
-                    A1.imprimir_almacen();
-                case 5:
-                    continuar=false;
-                    break;
-            }
-        }
-        return 0;
+    Calmacen A1=crear_almacen(cin);
+    A1.almacen_vacio();
+    A1.imprimir_almacen_vacio();
+    indicar_productos(A1,cin);
+    A1.imprimir_almacen();
+    indicar_robots(A1,cin);
+    A1.imprimir_almacen();
+    A1.usar_robot();
+    return 0;
 }
