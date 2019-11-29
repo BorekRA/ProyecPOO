d03e8f75efa88833cbee36c06b94413f9ff1cd85
BorekRA <56094849+BorekRA@users.noreply.github.com>
2019-11-28 16:52:37 -0800

    Update Crobot.cpp

--- Crobot.cpp
+++ Crobot.cpp
@@ -1,28 +1,6 @@
 #include "Crobot.h"
-#include "Cproducto.h"
-Crobot::Crobot(texto nam, t_posicion x, t_posicion y):name{nam}, pos_x{x}, pos_y{y} {
-    battery=100;
-    home[x][y];
-}
-
-t_ent Crobot::get_battery() {
-    return battery;
-}
-
-banderita Crobot::set_state(banderita stad) {
-    return state;
-}
-
-void Crobot::recharge_battery() {
-    if (battery==10) battery=100;
-}
-
-
-void Crobot::alternate_state() {
-    if (state != 0) state=false;
-    if (state == 0) state=true;
-}
-void Crobot::mover_robot(t_posicion x, t_posicion y){
-    pos_x=x; pos_y=y;
-    current_location[pos_x][pos_y];
+#include "tipos.h"
+void Crobot::set_posicion_r(valor x, valor y, vector<vector<texto>> s) {
+pos_x=x; pos_y=y;
+s[pos_x][pos_y];
 }
