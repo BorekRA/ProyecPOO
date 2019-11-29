c59b13103ee7f3be8c13d58c414b27f5bcad9add
MateoMalla772 <56094790+MateoMalla772@users.noreply.github.com>
2019-11-28 22:01:05 -0500

    Update Calmacen.h

--- Calmacen.h
+++ Calmacen.h
@@ -1,5 +1,5 @@
-#ifndef PROYECTO_POO_CALMACEN_H
-#define PROYECTO_POO_CALMACEN_H
+#ifndef PROYECPOOFINAL_CALMACEN_H
+#define PROYECPOOFINAL_CALMACEN_H
 #include "tipos.h"
 #include "Corden.h"
 #include "Cproducto.h"
@@ -8,8 +8,8 @@ class Calmacen {
     valor col;
     valor fil;
     vector<vector<texto>> slots;
-    Crobot* robot= nullptr;
-    Cproducto* producto= nullptr;
+    vector<Crobot*&>v;
+    vector<Cproducto*&> n;
 public:
     Calmacen(valor c,valor f);
     void imprimir_almacen_vacio();
@@ -20,9 +20,14 @@ public:
     void agregar_robot(Crobot rt);
     virtual ~Calmacen()=default;
     void imprimir_almacen();
-    void execute_operation(Crobot* r, Cproducto* p, vector<vector<texto>> s, valor x, valor y);
+    void rellenar_vector_robots(Crobot*& R);
+    void rellenar_vector_productos(Cproducto*& P);
+    void usar_robot();
 };
+
 Calmacen crear_almacen(istream& in);
 void indicar_productos(Calmacen& A1, istream& in);
 void indicar_robots(Calmacen& A1,istream& in);
-#endif //PROYECTO_POO2_CALMACEN_H
+
+
+#endif //PROYECPOOFINAL_CALMACEN_H
